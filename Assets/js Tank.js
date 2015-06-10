506d07a5376c516e431b7f9d3c271a7109ed5f42
leesuyun <2soooy@naver.com>
2015-06-10 16:45:56 +0900

    Unity file

---
+++ Assets/js Tank.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+var speed = 5;
+var rotSpeed = 120;
+var turret : GameObject;
+
+var power = 600;
+var bullet : Transform;
+
+function Update(){
+	var amtToMove = speed*Time.deltaTime;
+	var amtToRot = rotSpeed*Time.deltaTime;
+
+	var front= Input.GetAxis("Vertical");
+	var ang = Input.GetAxis("Horizontal");
+	var ang2 = Input.GetAxis("MyTank");
+
+	transform.Translate(Vector3.forward*front*amtToMove);
+	transform.Rotate(Vector3(0,ang*amtToRot,0));
+	turret.transform.Rotate(Vector3.up * ang2 * amtToRot);
+
+	if(Input.GetButtonDown("Fire1")){
+		var myBullet = Instantiate(bullet, turret.transform.position,turret.transform.rotation);
+		myBullet.GetComponent.<Rigidbody>().AddForce(turret.transform.forward*power);
+	}
+}
